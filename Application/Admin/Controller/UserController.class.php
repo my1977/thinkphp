8e0e9b564eadc2d2b02ea73bdc10e73adfd326e4
my1977 <1977905246@qq.com>
2016-05-20 21:10:56 +0800

    增删改查

--- Application/Admin/Controller/UserController.class.php
+++ Application/Admin/Controller/UserController.class.php
@@ -9,7 +9,7 @@ class UserController extends Controller {
 
     	$User = M('User'); // 实例化User对象
 		$count      = $User->where('status!=-1')->count();// 查询满足要求的总记录数
-		$Page       = new \Think\Page($count,1);// 实例化分页类 传入总记录数和每页显示的记录数(25)
+		$Page       = new \Think\Page($count,10);// 实例化分页类 传入总记录数和每页显示的记录数(25)
 		$Page->setConfig('prev',"上一页");
         $Page->setConfig('next','下一页');
         $Page->setConfig('header','<span class="rows">共 %TOTAL_ROW% 名用户</span>');
@@ -20,5 +20,37 @@ class UserController extends Controller {
 		$this->assign('user',$list);// 赋值数据集
 		$this->assign('page',$show);// 赋值分页输出
 		$this->display(); // 输出模板
+    }
+
+    public function add(){
+    	$this->display();
+    }
+
+    public function handleAdd(){
+    	$_POST['password'] = md5($_POST['password']);
+    	$_POST['create_time'] = time();
+    	M('User')->create();
+    	M('User')->add();
+    	$this->success('新增成功',U('admin/user/index'));
+    }
+
+    public function update(){
+    	$id = I('get.id',0);
+    	$user_info = M('User')->where("id = $id")->find();
+    	$this->assign('user_info',$user_info);
+    	$this->display();
+    }
+
+    public function handleUpdate(){
+    	$id = I('post.id',0);
+    	M('User')->create();
+    	M('User')->where("id = $id")->save();
+    	$this->success('修改成功',U('admin/user/index'));
+    }
+
+    public function del(){
+    	$User = M('User');
+    	$User->where(array('id'=>I('get.id',0)))->save(array('status'=>-1));
+    	$this->success('已删除',U('admin/user/index'));
     }
 }
