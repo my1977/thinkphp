7b2ee33851ff91a5f25fff586cda405b7e376224
my1977 <1977905246@qq.com>
2016-05-25 21:31:20 +0800

    无限分类

--- Application/Home/Controller/TestController.class.php
+++ Application/Home/Controller/TestController.class.php
@@ -5,26 +5,44 @@ use Think\Controller;
 class TestController extends Controller
 {
 	
+	//直接查数据库 好理解 缺点不能无限极分类 
 	function index(){
-		var_dump($list);
-	}
-	function add(){
-		echo "this id add";
+		header('content-type:text/html;charset=utf8');
+		$area = M('Areas')->where('parent_id = 0')->select();
+		foreach($area as $key=>$value){
+			$province = M('Areas')->where('parent_id = '.$value['area_id'])->select();
+			foreach($province as $k=>$v){
+				$city = M('Areas')->where('parent_id = '.$v['area_id'])->select();
+				foreach($city as $ck=>$cv){
+					$district = M('Areas')->where('parent_id = '.$cv['area_id'])->select();
+					$city[$ck]['child'] = $district;
+				}
+				$province[$k]['child'] = $city;
+			}
+			$area[$key]['child'] = $province;
+		}
+		echo "<pre>";
+		var_dump($area);
+		echo "</pre>";
 	}
 
-	function handleAdd(){
-		$data[''] = '123';
-		add();
+	//无限接分类 使用递归 需好好理解
+	function test(){
+		header('content-type:text/html;charset=utf8');
+		$area = F('area');
+		if (!is_array($area) || empty($area)) {
+			$area = M('Areas')->select();
+			$area = getTree($area);
+			F('area',$area);
+		}
+			
+		echo "<pre>";
+		var_dump($area);
+		echo "</pre>";
 	}
-	function update(){
-		echo "this is update";
+
+	function maying(){
+		//F('maying','love php');
+		echo F('maying');
 	}
-	function handleUpdate(){
-		$data[''] = '123';
-		save();
-	}
-	function delete(){
-		delete();
-	}
-	//新增两条  aaa->delete() bbb—>ccc
 }
